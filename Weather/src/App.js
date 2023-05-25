4cf5f7b171c7590f7cafb5d1d42ebeeec9714dbc
NoeliaGCalligaro <noeliacalligaro@gmail.com>
2023-05-25 01:44:20 -0300

    App2

---
+++ Weather/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import WeatherApp from "./components/weather/weatherApp";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Weather/src/components/weather/weatherApp.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./loading";
+import WeatherForm from "./weatherForm";
+import WeatherMainInfo from "./weatherMainInfo";
+import styles from "./weatherApp.module.css";
+
+export default function WeatherApp() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    loadInfo();
+  }, []);
+
+  async function loadInfo(city = "london") {
+    console.log(
+      `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+    );
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+      );
+      const json = await request.json();
+      console.log(json);
+
+      setTimeout(() => {
+        setWeather({ ...json });
+      }, 2000);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function handleOnChangeCity(city) {
+    setWeather(null);
+    loadInfo(city);
+  }
+
+  return (
+    <div className={styles.weatherContainer}>
+      <WeatherForm onChangeCity={handleOnChangeCity} />
+      {weather ? <WeatherMainInfo weather={weather} /> : <Loading />}
+    </div>
+  );
+}
+
